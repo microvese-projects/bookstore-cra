36689e55ddca4cafb2b3aa8a275a5100ebfe9a4c
Benson Njuguna <njugunab655@gmail.com>
2023-05-04 12:19:53 +0300

    Add navigation

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Categories from './components/Categories';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="categories" element={<Categories />} />
