c1ad14a596acc02a8ee0ad65ac58adc86fe35fbe
Benson Njuguna <njugunab655@gmail.com>
2023-05-18 11:54:19 +0300

    Hide form on loading

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import BookList from './Booklist';
 import Form from './Form';
 
 function Home() {
+  const { loading } = useSelector((state) => state.books);
+
   return (
     <>
       <BookList />
-      <Form />
+      {loading === false && <Form />}
     </>
   );
 }
