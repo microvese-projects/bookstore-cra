48d0174b71c1f3f1fccca56ae89c0b32238dd455
Benson Njuguna <njugunab655@gmail.com>
2023-05-19 13:26:30 +0300

    Capitalize word on adding :zap:

--- src/components/Book.js
+++ src/components/Book.js
@@ -7,12 +7,25 @@ import Status from './Status';
 function Book({
   item_id, title, author, category,
 }) {
+  const capitalize = (word) => {
+    const words = word.trim().split(' ');
+    const capitalizedWords = words.map((each) => {
+      const firstLetter = each.charAt(0).toUpperCase();
+      const restOfWord = each.slice(1);
+
+      return firstLetter + restOfWord;
+    });
+
+    return capitalizedWords.join(' ');
+  };
+
+  capitalize('ben Njuguna ');
   return (
     <li className="book">
       <div className="details">
         <h4 className="book-category">{category}</h4>
-        <h2 className="book-title">{title}</h2>
-        <h5 className="book-author">{author}</h5>
+        <h2 className="book-title">{capitalize(title)}</h2>
+        <h5 className="book-author">{capitalize(author)}</h5>
         <Buttons id={item_id} />
       </div>
       <Status />
