ca6d988b538daad59b60d50e9f5379d8f287a0f0
Benson Njuguna <njugunab655@gmail.com>
2023-05-19 08:42:13 +0300

    Add classnames

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Progress() {
   return (
     <div className="progress">
-      <h5>CURRENT CHAPTER</h5>
-      <h4>Chapter 17</h4>
+      <h5 className="current-chapter">CURRENT CHAPTER</h5>
+      <h4 className="chapter-description">Chapter 17</h4>
       <button type="button">UPDATE PROGRESS</button>
     </div>
   );
